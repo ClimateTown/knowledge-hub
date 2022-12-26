dfd2c4acc1226fcb540fcd41069fd437e1900626
Vecko <VeckoTheGecko@users.noreply.github.com>
2022-12-26 13:07:42 +0800

    Add resource loading from YAML

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,6 +4,33 @@
 declare namespace App {
 	// interface Error {}
 	// interface Locals {}
-	// interface PageData {}
+
+	// TODO: Need to learn how to use these interfaces
+	interface PageData {
+		payload: {
+			resources: [
+				{
+					title: string,
+					url: string,
+					summary: string,
+					tags: string[],
+				}
+			]
+		}
+	}
+
+	interface PageDataYouTube {
+		resources: [
+			{
+				title: string,
+				channel_name: string,
+				channel_username: string,
+				channel_id: string,
+				upload_date: Date,
+			}
+		]
+	}
 	// interface Platform {}
 }
+
+
