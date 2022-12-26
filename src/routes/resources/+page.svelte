1397faead98fdd3286afeb0fcf2acd240a4a8019
Vecko <VeckoTheGecko@users.noreply.github.com>
2022-12-26 22:12:15 +0800

    Implemented resource feed v1

---
+++ src/routes/resources/+page.svelte
@@ -0,0 +1,14 @@
+<!-- import PageData type -->
+<script lang="ts">
+    import type { PageData } from "./$types";
+
+    export let data: PageData;
+
+    let resources = data.payload.resources;
+
+    import ListItem from "./ListItem.svelte";
+</script>
+
+{#each resources as resource}
+    <ListItem {...resource} />
+{/each}
---
+++ src/routes/resources/ListItem.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let title;
+    export let url;
+    export let description;
+    export let tags;
+</script>
+
+<div>
+    <a href={url}>{title}</a>
+    <p>
+        {description}
+    </p>
+    {#each tags as tag}
+        <span>{tag}</span>
+    {/each}
+</div>
+
+<style>
+    div {
+        border-style: solid;
+        margin: 1em;
+    }
+    span {
+        margin: 1em;
+    }
+</style>
