ff70415457269591047479feac5044b44902c9ae
Vecko <VeckoTheGecko@users.noreply.github.com>
2022-12-27 23:35:59 +0800

    Removing search engine indexing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,9 @@
+<svelte:head>
+    <title>My App</title>
+    <!-- TODO: Remove line in production -->
+    <meta name="robots" content="noindex" />
+</svelte:head>
+
 <nav>
     <a href="/">Home</a>
     <a href="/resources">Resources</a>
