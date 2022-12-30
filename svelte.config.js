24ad17ad74107ac044bc4da4758ae1f769b6dfb4
Vecko <VeckoTheGecko@users.noreply.github.com>
2022-12-30 11:32:58 +0800

    Add preprocess to config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 
 // Adapted from [Stack overflow](https://stackoverflow.com/questions/72730192/how-to-host-a-sveltekit-adapter-static-project-on-github-pages)
 // and [adapter-static](https://github.com/sveltejs/kit/tree/master/packages/adapter-static#github-pages)
@@ -6,6 +8,7 @@ const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
     kit: {
         adapter: adapter({
             pages: 'build',
