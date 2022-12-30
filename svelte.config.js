75806d09c076a81329e2e1cd204da138fa1943e4
Vecko <VeckoTheGecko@users.noreply.github.com>
2022-12-30 11:12:21 +0800

    Switch adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+
+// Adapted from [Stack overflow](https://stackoverflow.com/questions/72730192/how-to-host-a-sveltekit-adapter-static-project-on-github-pages)
+// and [adapter-static](https://github.com/sveltejs/kit/tree/master/packages/adapter-static#github-pages)
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false
+        }),
+		prerender: {
+			entries: ['*']
+		},
+        paths: {
+            base: dev ? '' : '/knowledge-hub'
+        }
+    }
 };
 
 export default config;
+
