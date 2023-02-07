04ddace10de9dfa8655bafebf38f6a86ee791450
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2023-02-07 09:37:54 +0800

    Update YouTube feed styling (#50)
    * Tailwind import
    
    * Styling of YouTube feed
    
    ---------
    
    Co-authored-by: Vecko <VeckoTheGecko@users.noreply.github.com>

--- src/routes/youtube/YoutubeVideo.svelte
+++ src/routes/youtube/YoutubeVideo.svelte
@@ -1,34 +1,61 @@
 <script lang="ts">
     export let title: string;
     export let videoId: string;
-    export let realeasedDatetime: string;
     export let channelInfo: any; // Contains channel data for the video uploader
+    // export let realeasedDatetime: string;
 
     const thumbnail = `https://i.ytimg.com/vi/${videoId}/default.jpg`;
     const { channelCustomName, channelName } = channelInfo;
 
-    function trimText(text: string, maxLength: number) {
-        if (text.length > maxLength) {
-            return text.slice(0, maxLength) + "...";
+    function trimString(str: string, maxLength: number): string {
+        /**
+         * Trims a string to a max length, finding a space character in order to end the string.
+         */
+
+        // Check if the length of the input string is already less than or equal to maxLength
+        if (str.length <= maxLength) {
+            return str;
         }
-        return text;
+
+        // Trim the string to maxLength characters
+        let trimmedString = str.substring(0, maxLength);
+
+        // Find the last occurrence of a space character in the trimmed string
+        let lastIndex = trimmedString.lastIndexOf(" ");
+
+        // If a space character is found, trim the string again to remove everything after the space character
+        if (lastIndex !== -1) {
+            trimmedString = trimmedString.substring(0, lastIndex);
+        }
+
+        // Add an ellipsis to indicate that the string has been trimmed
+        return trimmedString + "...";
     }
 </script>
 
-<div>
-    <img src={thumbnail} alt="{title} thumbnail" /><br />
-    <a href="https://youtu.be/{videoId}">{trimText(title, 50)}</a> <br />
-    <a href="https://www.youtube.com/{channelCustomName}">{channelName}</a>
-    <br />
-    <!-- <p>realeasedDatetime</p> -->
-</div>
-
-<style>
-    div {
-        border-style: solid;
-        margin: 1em;
-    }
-    img {
-        width: 5em;
-    }
-</style>
+<a href="https://youtu.be/{videoId}" target="_blank" rel="noreferrer">
+    <div
+        class="flex w-full flex-row md:block md:w-32 md:h-44 shadow-md rounded-lg"
+    >
+        <img
+            class="rounded-l-lg md:rounded-bl-none md:rounded-t-lg md:w-full"
+            src={thumbnail}
+            alt="{title} thumbnail"
+        />
+        <div class="mx-2">
+            <!-- Don't trim title on mobile, trim on desktop (probably a better way to do this with media queries) -->
+            <div class="hidden md:block text-sm font-semibold">
+                {@html trimString(title, 30)}
+            </div>
+            <div class="md:hidden text-sm font-semibold">{@html title}</div>
+            <a
+                class="text-sm text-gray-500"
+                href="https://www.youtube.com/{channelCustomName}"
+                target="_blank"
+                rel="noreferrer"
+            >
+                {@html channelName}</a
+            >
+        </div>
+    </div>
+</a>
