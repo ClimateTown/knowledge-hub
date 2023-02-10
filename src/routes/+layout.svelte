a8787b057ada7e3639bc3fed1e57742ea8da0bea
Vecko <VeckoTheGecko@users.noreply.github.com>
2023-02-11 00:50:57 +0800

    Styling: navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,16 +16,17 @@
     ></script>
 </svelte:head>
 
-<div class="body flex flex-col lg:flex-row min-w-fit">
-    <div class="w-full lg:w-1/5 bg-green-600 flex flex-col lg:p-5 min-h-max lg:min-h-screen px-16">
-        <img class="md:w-6/12 lg:w-full self-center" src="{base}/images/knowledge-hub-logo.png" alt="Logo of Knowledge Hub" />
-        <nav class="self-center gap-4 flex flex-wrap lg:flex-col pb-6 lg:pb-0">
-            <button class="bg-slate-200 p-3 drop-shadow-md"><a href="{base}/">Home</a></button>
-            <button class="bg-slate-200 p-3 drop-shadow-md"><a href="{base}/resources">Resources</a></button>
-            <button class="bg-slate-200 p-3 drop-shadow-md"><a href="{base}/youtube">YouTube Feed</a></button>
-            <button class="bg-slate-200 p-3 drop-shadow-md"><a href="https://github.com/ClimateTown/knowledge-hub">✍ Contribute on GitHub</a></button>
-            <button class="bg-slate-200 p-3 drop-shadow-md"><a href="https://github.com/ClimateTown/knowledge-hub/graphs/contributors">📣 Credits</a></button>
-            <button class="bg-slate-200 p-3 drop-shadow-md"><a href="https://www.climatetownproductions.com/">🌐 Main Website </a></button>
+<div class="body flex flex-col lg:flex-row w-full">
+    <div class="lg:w-fit w-full bg-green-500 flex flex-col p-5 min-h-max lg:min-h-screen">
+        <img class="w-48 self-center" src="{base}/images/knowledge-hub-logo.png" alt="Logo of Knowledge Hub" />
+        <nav class="gap-2 flex flex-wrap lg:flex-col">
+            <a class="p-2 text-slate-200 bg-green-600 font-bold rounded-lg" href="{base}/">Home</a>
+            <a class="p-2 text-slate-200 bg-green-600 font-bold rounded-lg" href="{base}/resources">Resources</a>
+            <a class="p-2 text-slate-200 bg-green-600 font-bold rounded-lg" href="{base}/youtube">YouTube Feed</a>
+            <div class="h-4"></div>
+            <a class="p-2 text-slate-200 bg-green-600 font-bold rounded-lg" target="_blank" rel="noreferrer" href="https://github.com/ClimateTown/knowledge-hub">✍ Contribute on GitHub</a>
+            <a class="p-2 text-slate-200 bg-green-600 font-bold rounded-lg" target="_blank" rel="noreferrer" href="https://github.com/ClimateTown/knowledge-hub/graphs/contributors">📣 Credits</a>
+            <a class="p-2 text-slate-200 bg-green-600 font-bold rounded-lg" target="_blank" rel="noreferrer" href="https://www.climatetownproductions.com/">🌐 Main Website </a>
         </nav>
     </div>
 
@@ -41,5 +42,6 @@
         width: 1em;
         margin: 0 0.05em 0 0.1em;
         vertical-align: -0.1em;
+        display: inline;
     }
 </style>
