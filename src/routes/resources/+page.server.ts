9d3e6905e26c7f9144fc8cae54d98167b7ff9669
isidro <isidromolina@pursuit.org>
2023-05-09 16:11:43 -0400

    add sorting fn to tags

--- src/routes/resources/+page.server.ts
+++ src/routes/resources/+page.server.ts
@@ -8,14 +8,28 @@ let resources_path: string = "data/resources.yml"
 const file = fs.readFileSync(resources_path, 'utf8')
 const yml_data = parse(file) // TODO: Create interface for this, and then use it to validate edits during CI/CD
 
+const removeEmojisAndTrim = (str: string) => {
+  let emojis = /<a?:.+?:\d{18}>|\p{Extended_Pictographic}/gu;
+  return str.replace(emojis, '').trim()
+}
 // Creating a list of unique tags
 let tags: string[] = [];
 let resource;
 for (resource of yml_data) {
   tags.push(...resource.tags)
 }
-tags = [...new Set(tags)]
-
+tags = [...new Set(tags)].sort((a, b) =>{
+  const aTagWithoutEmoji = removeEmojisAndTrim(a);
+  const bTagWithoutEmoji = removeEmojisAndTrim(b);
+  
+  if (aTagWithoutEmoji[0].toLowerCase() < bTagWithoutEmoji[0].toLowerCase()) {
+    return -1;
+  }
+  if (aTagWithoutEmoji[0].toLowerCase() > bTagWithoutEmoji[0].toLowerCase()) {
+    return 1;
+  }
+  return 0;
+})
 //Sorting resources by newest (assuming newest is at the bottom of the file )
 const yml_data_sort = [...yml_data].reverse();
 
