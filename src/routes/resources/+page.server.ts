8defd53ee0c210aad4dfd445e258f730e964216a
JORDAN MANLEY <jordanmanley@JORDANs-MacBook-Pro.local>
2023-05-09 21:44:44 -0400

    set tags in alphabetical order, ignoring emojis

--- src/routes/resources/+page.server.ts
+++ src/routes/resources/+page.server.ts
@@ -2,28 +2,54 @@ import type { PageServerLoad } from './$types'
 import { parse } from 'yaml'
 import fs from 'fs'
 
-let resources_path: string = "data/resources.yml"
-
-// Reading in resources
-const file = fs.readFileSync(resources_path, 'utf8')
-const yml_data = parse(file) // TODO: Create interface for this, and then use it to validate edits during CI/CD
-
-// Creating a list of unique tags
-let tags: string[] = [];
-let resource;
-for (resource of yml_data) {
-  tags.push(...resource.tags)
+interface Resource {
+  title: string,
+  description: string,
+  url: string,
+  tags: string[]
 }
-tags = [...new Set(tags)]
 
-//Sorting resources by newest (assuming newest is at the bottom of the file )
-const yml_data_sort = [...yml_data].reverse();
+const parseResourcesFromYaml = (filePath: string) => {
+  const file = fs.readFileSync(filePath, 'utf8')
+  const resources: Resource[] = parse(file)
+  return resources;
+}
+ 
+const generateUniqueTags = (resources: Resource[]) => {
+  const tags: string[] = []
+
+  for (const resource of resources) {
+    tags.push(...resource.tags);
+  }
+
+  return [...new Set(tags)]
+}
+
+const removeEmojisFromStr = (str: string) => {
+  return str.replace(/[\u1000-\uFFFF]+/g, '').trim();
+}
+
+const sortAlphabeticallyIgnoringEmojis = (a: string, b: string) => {
+  const aWithoutEmojis = removeEmojisFromStr(a);
+  const bWithoutEmojis = removeEmojisFromStr(b);
+
+  return aWithoutEmojis.localeCompare(bWithoutEmojis);
+}
 
 export function load(params: PageServerLoad) {
+
+  const resources: Resource[] = parseResourcesFromYaml('data/resources.yml');
+
+  const uniqueTags: string[] = generateUniqueTags(resources);
+
+  const uniqueTagsInAlphabeticalOrder = uniqueTags.sort(sortAlphabeticallyIgnoringEmojis);
+
+  const resourcesNewestToOldest = [...resources].reverse();
+
   return {
     payload: {
-      resources: yml_data_sort,
-      tags: tags,
+      resources: resourcesNewestToOldest,
+      tags: uniqueTagsInAlphabeticalOrder,
     }
   }
 }
