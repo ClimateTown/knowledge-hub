fd380b962f785fad582066b05b1cf77f9ab15f81
JORDAN MANLEY <jordanmanley@JORDANs-MacBook-Pro.local>
2023-05-14 19:08:02 -0700

    import yaml to test file

--- scripts/validate_tags.py
+++ scripts/validate_tags.py
@@ -1,24 +1,31 @@
 from pathlib import Path
 
+import yaml
+from loguru import logger
+
 resource_tags_file = Path("data") / "resource_tags.yml"
 resources_file = Path("data") / "resources.yml"
 
 def main():
     with resource_tags_file.open() as f:
-        tags = yaml.safe_load(f)
+        tagObjects = yaml.safe_load(f)
     logger.success("Read in `resource_tags.yml` file.")
 
     with resources_file.open() as f:
         resources = yaml.safe_load(f)
     logger.success("Read in `resources.yml` file.")
 
-    for tag in resource_tags_file:
-        logger.info(f"Getting of {tag['name']}")
+    # for tag in tags:
+    #     logger.info(f"Getting of {tag['name']}")
 
-    #for each resource
+    tags = [tag['name'] for tag in tagObjects]
+
+    logger.info(f"{tags}")
+    
     for resource in resources:
         # check that each tag is in the list of tags
-        for tag in resource.tags:
+        for tag in resource['tags']:
+            logger.info(f"{tag} from {resource['title']}")
             if tag not in tags:
                 logger.error(f"{tag} not in list of tags")
                 raise Exception(f"{tag} not in list of tags")
