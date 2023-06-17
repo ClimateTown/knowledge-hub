eba1c9a18b609b8f5b3f6fb7b58948402224a891
isidro <isidromolina@pursuit.org>
2023-06-16 20:41:57 -0400

    move Channel, ChannelFilterItem and Video interfaces into interfaces.ts file

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -10,3 +10,18 @@ export interface Tag {
   name: string;
   color?: string;
 }
+
+export interface Channel {
+  channelId: string;
+  channelSubCount: number;
+}
+
+export interface ChannelFilterItem { 
+  channelId: string;
+  active: boolean;
+}
+
+export interface Video {
+  channelId: string;
+  [key: string]: any;
+}
